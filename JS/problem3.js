3d30aa37c6a368967789bcc3419faed13c8d4416
jpa1994 <john.astvazaturov@mscoding.org>
2025-04-22 12:03:32 -0500

    Update problem3.js

--- JS/problem3.js
+++ JS/problem3.js
@@ -4,7 +4,7 @@ const start = 1; // Same as (let i = 1; i <= 100) but can easily change const fo
 const end = 100;
 
 for (let i = start; i <= end; i++){
-    if((i % 7) == 0){
+    if((i % 7) === 0){
         console.log('First number divisible by 7 is:', i);
         break;
     }
